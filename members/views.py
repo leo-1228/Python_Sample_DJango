f445ae4a6f35945d968736691ae1107dee7ee3e3
leo-1228 <akiraito731@gmail.com>
2024-06-04 11:36:05 -0400

    add my template

--- members/views.py
+++ members/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def members(request):
-    return HttpResponse("Hello world!")
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
