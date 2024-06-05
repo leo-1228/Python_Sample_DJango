ad500090e1fdeb14bc2180d1c2eaab4e38ed67d3
leo-1228 <akiraito731@gmail.com>
2024-06-05 08:06:49 -0400

    reset setting to redirect to 404 page

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8qiuxf+8c(8eo!gzp0ed!bf3%hs+*+da@@td$j80ml(7xop=91'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
